8a0e618932b5a406d391c335c5eabdb083a31f72
Pedro Freitas <freitaas.pedro@gmail.com>
2023-07-28 12:02:15 -0300

    done a few changes in front-end

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import './Main.css';
 import weatherCodeIcon from "./weatherJson"
 import { HiOutlineThumbUp } from "react-icons/hi";
 import { WiAlien } from 'react-icons/wi';
+import { BsCompass, BsSpeedometer2 } from "react-icons/bs";
+import { TbTemperature } from "react-icons/tb";
+import { CiGps } from "react-icons/ci";
 
 function WeatherIcon(weatherJson) {
   if(!weatherJson.defined)
     return (<div></div>);
 
+  let icon = 0;
+
   try {
-    return weatherCodeIcon[weatherJson.weather.code];
+    icon = weatherCodeIcon[weatherJson.weather.code];
   } catch {
-    return <WiAlien size={50} color="000"/>
+    icon = <WiAlien size={50} color="000"/>;
   }
+  
+  return (
+    <div className="weatherIcon"> 
+      {icon} 
+    </div>
+  );
 }
 
 function MainAPIError() {
@@ -27,12 +38,17 @@ function MainAPIError() {
 function Main(weatherJson) {
   return (
     <main>
-      <h2> {weatherJson.city_name} </h2>
-      <span> {weatherJson.datetime} </span>
-      <span> Coordinates: ({weatherJson.lat}, {weatherJson.lon}) </span>
-      <span> Temperature: {weatherJson.app_temp} </span>
-      <span> Wind's direction: {weatherJson.wind_cdir} </span>
-      <span> Wind's speed: {weatherJson.wind_spd} m/s </span>
+      <div className='main_header'> 
+        <h2> {weatherJson.city_name} </h2> 
+        {WeatherIcon(weatherJson)} 
+      </div>
+      <span className="date_span"> {weatherJson.datetime} </span>
+      <span> <CiGps size={35}/> ({weatherJson.lat}, {weatherJson.lon}) </span>
+      <span> <TbTemperature size={35}/> {weatherJson.app_temp}°C </span>
+      <span className='wind_info'> <h3>Wind</h3>
+        <div className='wind_dir'> <BsCompass size={30}/> {weatherJson.wind_cdir} </div>
+        <div className='wind_speed'> <BsSpeedometer2 size={30}/> {weatherJson.wind_spd} m/s </div>
+      </span>
     </main>
   );
 }
@@ -42,20 +58,24 @@ function App() {
     defined: false,
   });
 
-  // animate main after every weather search.
+  // animate main/weatherIcon after every weather search.
   useEffect( () => {
     function changeMainAnimation(animationName) {
       const main = document.getElementsByTagName("main")[0];
+      if(main !== undefined) main.className=animationName;
+    }
 
-      if(main === undefined) return;
-
-      main.className=animationName;
+    function changeWeatherIconAnimation(animationName) {
+      const weatherIcon = document.getElementsByClassName("weatherIcon")[0];
+      if(weatherIcon !== undefined) weatherIcon.className=`weatherIcon ${animationName}`;
     }
 
     changeMainAnimation("animationX");
+    changeWeatherIconAnimation("animationY");
 
     setTimeout( () => {
       changeMainAnimation("");
+      changeWeatherIconAnimation("");
     }, 1000);
 
   }, [weatherJson]);
@@ -64,7 +84,7 @@ function App() {
     <div 
     className="App">
       <div className="searchBox">
-        <h2> Get Weather </h2>
+        <h2> Get Weather </h2> 
         <div className="inputs">
           <input id="input" type="text" placeholder="Digit city name."></input>
           <button id="button" onClick={async () => {updateWeatherJson(await getWeatherJson())}}> <HiOutlineThumbUp size={30} color="#000"/> </button>
@@ -74,8 +94,6 @@ function App() {
       {weatherJson.defined && weatherJson.api_ok && Main(weatherJson)}
 
       {weatherJson.defined && !weatherJson.api_ok && <MainAPIError/>}
-    
-      <WeatherIcon weatherJson={weatherJson}/>
 
     </div>
   );
@@ -89,7 +107,24 @@ function getInputValue() {
   return value;
 }
 
+let count = 0;
 async function getWeatherJson() {
+  // just to avoid depleting API.
+  count++;
+  return {
+    defined: true, api_ok: true,
+    city_name: "Guadalajara" + String(count),
+    datetime: "2023-07-28 10:52",
+    lat: -10, lon: -200,
+    app_temp: 32.8,
+    wind_cdir: "O",
+    wind_spd: 14.2,
+  
+    weather: {
+      code: 201,
+    }
+  }
+
   const cityName = getInputValue();
   const apiKey = process.env.REACT_APP_API_KEY;
 
